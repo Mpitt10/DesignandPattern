f54c5106ff770351f8a7b60c97a2fa5348440784
Mpitt10 <84051955+Mpitt10@users.noreply.github.com>
2023-08-26 22:40:37 -0500

    color and button on bottom

--- AppCanvas.java
+++ AppCanvas.java
@@ -12,6 +12,10 @@ public class AppCanvas extends JPanel {
 
     @Override
     public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        var font = new Font(name:"Courier New", Font.BOLD, size:24);
+        g.setFont(font);
+        g.setColor(Color.BLUE);
         g.drawString(str:"Lucky Number Is:", x:30, y:100);
         g.drawString("" + randomNumber, x:30, y:140);
     }
--- AppWindow.java
+++ AppWindow.java
@@ -9,5 +9,15 @@ public class AppWindow extends JFrame {
 
         var canvas = new AppCanvas();
         cp.add(canvas, BorderLayout.CENTER);
+
+        JButton button = new JButton(text:"Lucky Number");
+        cp.add(button, BorderLayout.SOUTH);
+
+        button.addActionListener(e -> {
+            var r = new Random();
+            int n = r.nextInt(bound:101);
+            canvas.randomNumber = n;
+            canvas.repaint();
+        });
     }
 }
