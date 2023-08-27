ff5c8951dbe9fb63c5b1507fcd1714344f101706
Mpitt10 <84051955+Mpitt10@users.noreply.github.com>
2023-08-26 22:11:39 -0500

    Start of lesson1

---
+++ App.java
@@ -0,0 +1,10 @@
+public class App {
+
+    public static void main(String[] args) {
+        AppWindow win = new AppWindow();
+        win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        win.init();
+
+        win.setVisible(b:true);
+    }
+}
---
+++ AppWindow.java
@@ -0,0 +1,8 @@
+import javax.swing.JFrame;
+
+public class AppWindow extends JFrame {
+
+    public void init() {
+        setTitle(title:"My first Java Window");
+    }
+}
